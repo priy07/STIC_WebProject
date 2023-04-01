5e37550149c5a026bd58d99edeaaf218d87178f9
Mohit Namdev <namdevmohit0@gmail.com>
2023-04-02 01:09:30 +0530

    Added all necessary files ( Mohit )

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Gallery from "./Components/Gallery.svelte";
+import HomePage from "./Components/HomePage.svelte";
+import Membership from "./Components/Membership.svelte";
+import Mentor from "./Mentor.svelte";
+
+    
+</script>
+<main class="bg-[#0E0D0D] text-white">
+<HomePage/>
+<Mentor/>
+<Gallery/>
+<Membership/>
+</main>
---
+++ src/routes/Components/HomePage.svelte
@@ -0,0 +1,17 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter&family=Outfit:wght@400;500;700&display=swap');
+
+.HomePage{
+  background-color: #0E0D0D ;
+}
+
+    </style>
+<main>
+    <div class="HomePage align-middle text-center h-[700px] pt-32">
+        <h1 class="font-outfit font-medium text-5xl leading-none text-white mb-12">Medi-Caps University</h1>
+        <h1 class="font-outfit font-bold text-[100px] leading-140 text-transparent bg-gradient-to-r from-blue-800 to-cyan-300 bg-clip-text text-center leading-[120%]">Student’s Technical and<br> Innovation Club </h1>
+        <h1 class="font-inter font-medium text-[21px] text-base leading-7 text-center text-gray-400 my-4">Ideas Today, Innovation Tomorrow</h1>
+        <button class="border-2 border-cyan-400 rounded-lg mt-4"><h1 class="font-inter text-[21px] font-semibold text-base leading-7 text-center text-gray-400 hover:bg-cyan-300 hover:text-gray-700 transition-all px-14 py-4">Join Us</h1></button>
+
+    </div>
+</main>
