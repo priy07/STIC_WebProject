3559f850ea7b5d99defe2597b62611e1a234071c
Aradhya Tiwari <aradhya.prad@gmail.com>
2023-04-02 16:22:10 +0530

    made metor component responsive

--- src/lib/Components/Mentor.svelte
+++ src/lib/Components/Mentor.svelte
@@ -1,9 +1,9 @@
 <div class=" ">
 	<!-- nikita mam wala section-->
 	<h1 class="text-7xl text-center pt-16">Chief Advisor</h1>
-	<div class="flex justify-between w-full p-20 relative">
-		<div class="bg[url()] w-[40%] m-auto h-80 border border-slate-400" />
-		<div class="w-[50%]">
+	<div class="flex flex-col sm:flex-row justify-between w-full p-20 relative">
+		<div class="bg[url()] rounded-lg sm:w-[40%] w-full m-auto h-80 border border-slate-400" />
+		<div class="sm:w-[50%] m-auto w-full">
 			<h2 class="text-5xl p-5">Prof.Nitika Vats Doohan</h2>
 			<p class="text-justify p-5">
 				In this new era of advancements and striding researches in new directions of engineering
@@ -21,8 +21,8 @@
 	<!--Dusre Sir wala section -->
 	<h1 class="text-7xl text-center pt-16">Faculty Co-ordinator</h1>
 
-	<div class="flex justify-between w-full p-20 relative">
-		<div class="w-[50%]">
+	<div class="flex flex-col sm:flex-row justify-between rounded-lg sm:w-[40%] w-full p-20 relative">
+		<div class="sm:w-[50%] w-full m-auto">
 			<h2 class="text-5xl p-5">Prof. Arpit Deo</h2>
 			<p class="text-justify p-5">
 				In this new era of advancements and striding researches in new directions of engineering
@@ -36,6 +36,6 @@
 		<div
 			class="top-10 left-0 gradient-radial absolute bg-gradient-to-tl blur-3xl overflow-hidden from-cyan-500 rounded-full w-[30%] h-[50%] opacity-70"
 		/>
-		<div class="bg[url()] w-[40%] m-auto h-80 border border-slate-400" />
+		<div class="bg[url()] w-full sm:w-[40%] m-auto h-80 border border-slate-400" />
 	</div>
 </div>
