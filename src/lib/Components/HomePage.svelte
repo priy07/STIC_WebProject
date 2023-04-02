6ef61ef519cc10a0cfe91b71bdfd5a9cd5f9a7b4
Mohit Namdev <namdevmohit0@gmail.com>
2023-04-02 15:42:52 +0530

    added responsiveness to homepage

--- src/lib/Components/HomePage.svelte
+++ src/lib/Components/HomePage.svelte
@@ -6,12 +6,12 @@
 }
 
     </style>
-<main>
+<main class="mt-10">
     <div class="HomePage align-middle text-center h-[700px] pt-32">
-        <h1 class="font-outfit font-medium text-5xl leading-none text-white mb-12 lg:text-3xl ">Medi-Caps University</h1>
-        <h1 class="font-outfit font-bold text-[100px] leading-140 text-transparent bg-gradient-to-r from-blue-800 to-cyan-300 bg-clip-text text-center leading-[120%]">Student’s Technical and<br> Innovation Club </h1>
-        <h1 class="font-inter font-medium text-[21px] text-base leading-7 text-center text-gray-400 my-4">Ideas Today, Innovation Tomorrow</h1>
-        <button class="border-2 border-cyan-400 rounded-lg mt-4"><h1 class="font-inter text-[21px] font-semibold text-base leading-7 text-center text-gray-400 hover:bg-cyan-300 hover:text-gray-700 transition-all px-14 py-4">Join Us</h1></button>
+        <h1 class="font-outfit font-medium text-xl leading-none text-white mb-12 lg:text-5xl md:text-4xl sm:text-3xl">Medi-Caps University</h1>
+        <h1 class="font-outfit font-bold text-4xl leading-140 text-transparent bg-gradient-to-r from-blue-800 to-cyan-300 bg-clip-text text-center leading-[120%] lg:text-8xl md:text-7xl sm:text-5xl">Student’s Technical and<br> Innovation Club </h1>
+        <h1 class="font-inter font-medium lg:text-[21px] sm:text-[15px] text-[10px] text-base leading-7 text-center text-gray-400 my-4">Ideas Today, Innovation Tomorrow</h1>
+        <button class="border-2 border-cyan-400 rounded-lg mt-4"><h1 class="font-inter lg:text-[21px] sm:text-[15px] text-[10px] font-semibold text-base leading-7 text-center text-gray-400 hover:bg-cyan-300 hover:text-gray-700 transition-all lg:px-14 lg:py-4 md:px-10 md:py-3 sm:px-7 sm:py-2 px-5 py-1">Join Us</h1></button>
 
     </div>
 </main>
