0ee56924a3ccb64df1086f5904762dff3ba9e9c6
Mohit Namdev <namdevmohit0@gmail.com>
2023-04-02 15:12:08 +0530

    added divison

--- src/lib/Components/HomePage.svelte
+++ src/lib/Components/HomePage.svelte
@@ -8,7 +8,7 @@
     </style>
 <main>
     <div class="HomePage align-middle text-center h-[700px] pt-32">
-        <h1 class="font-outfit font-medium text-5xl leading-none text-white mb-12">Medi-Caps University</h1>
+        <h1 class="font-outfit font-medium text-5xl leading-none text-white mb-12 lg:text-3xl ">Medi-Caps University</h1>
         <h1 class="font-outfit font-bold text-[100px] leading-140 text-transparent bg-gradient-to-r from-blue-800 to-cyan-300 bg-clip-text text-center leading-[120%]">Student’s Technical and<br> Innovation Club </h1>
         <h1 class="font-inter font-medium text-[21px] text-base leading-7 text-center text-gray-400 my-4">Ideas Today, Innovation Tomorrow</h1>
         <button class="border-2 border-cyan-400 rounded-lg mt-4"><h1 class="font-inter text-[21px] font-semibold text-base leading-7 text-center text-gray-400 hover:bg-cyan-300 hover:text-gray-700 transition-all px-14 py-4">Join Us</h1></button>
