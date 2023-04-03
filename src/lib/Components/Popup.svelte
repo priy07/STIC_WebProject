94c540b4398f7a2bab6618cc95fa21f527f1c223
Mohit Namdev <namdevmohit0@gmail.com>
2023-04-03 15:12:53 +0530

    popup fixed

--- src/lib/Components/Popup.svelte
+++ src/lib/Components/Popup.svelte
@@ -1,7 +1,7 @@
 <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full   overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full bg-transparent">
     <div class="relative w-full h-full max-w-md md:h-auto bg-transparent">
         <!-- Modal content -->
-        <div class="relative rounded-lg shadow bg-gray-700 top-[150px] xl:left-[500px] lg:[300px] md:left-[300px] sm:left-[200px] mx-[20px]">
+        <div class="relative rounded-lg shadow bg-gray-700 top-[130px] xl:left-[500px] lg:[300px] md:left-[300px] sm:left-[200px] mx-[20px]">
             <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Close modal</span>
