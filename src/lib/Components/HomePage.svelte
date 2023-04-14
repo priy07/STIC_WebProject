4c379413f392a656420a9ccae6fce1d2248ac509
Mohit Namdev <namdevmohit0@gmail.com>
2023-04-14 14:47:45 +0530

    linked the Join us button to membership

--- src/lib/Components/HomePage.svelte
+++ src/lib/Components/HomePage.svelte
@@ -14,7 +14,7 @@
         <h1 class="font-outfit font-medium text-xl leading-none text-white mb-12 lg:text-5xl md:text-4xl sm:text-3xl">Medi-Caps University</h1>
         <h1 id="heading"class="font-outfit font-bold text-4xl leading-140 text-transparent bg-gradient-to-r from-blue-800 to-cyan-300 bg-clip-text text-center leading-[120%] lg:text-8xl md:text-7xl sm:text-5xl">Student’s Technical and<br> Innovation Club </h1>
         <h1 class="font-inter font-medium lg:text-[21px] sm:text-[15px] text-[10px] text-base leading-7 text-center text-gray-400 my-4">Ideas Today, Innovation Tomorrow</h1>
-        <button class="border-2 border-cyan-400 rounded-lg mt-4"><h1 class="font-inter lg:text-[21px] sm:text-[15px] text-[10px] font-semibold text-base leading-7 text-center text-gray-400 hover:bg-cyan-300 hover:text-gray-700 transition-all lg:px-14 lg:py-4 md:px-10 md:py-3 sm:px-7 sm:py-2 px-5 py-1">Join Us</h1></button>
+        <button class="border-2 border-cyan-400 rounded-lg mt-4"><h1 class="font-inter lg:text-[21px] sm:text-[15px] text-[10px] font-semibold text-base leading-7 text-center text-gray-400 hover:bg-cyan-300 hover:text-gray-700 transition-all lg:px-14 lg:py-4 md:px-10 md:py-3 sm:px-7 sm:py-2 px-5 py-1"><a href="#member">Join Us</a></h1></button>
 
     </div>
 </main>
--- src/lib/Components/Membership.svelte
+++ src/lib/Components/Membership.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<main>
+<main id="member">
     <section class="bg-[#0E0D0D]">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div class="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
