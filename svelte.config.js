417e9fc34eef812d55a4fc05bd5ecbf79f4a44ed
Aradhya Tiwari <aradhya.prad@gmail.com>
2023-04-26 23:39:44 +0530

    admin area has to be deleted

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter()
+        adapter: adapter({
+            edge: true
+        })
     },
 
     preprocess: [preprocess({
