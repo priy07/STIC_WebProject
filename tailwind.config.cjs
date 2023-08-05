68cc9e273e44363f6907680a35ef39659a9e9322
SticMedicapsUniversity <130895359+SticMedicapsUniversity@users.noreply.github.com>
2023-08-05 15:04:04 +0530

    tailwind config file moved to tailwind.config.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,8 +11,19 @@ const config = {
       'outfit':['Outfit'],
     }
   },
-
+  darkMode: "class",
+    theme: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+        mono: ["ui-monospace", "monospace"],
+      },
+    },
+    corePlugins: {
+      preflight: false,
+    },
   plugins: []
 };
 
 module.exports = config;
+
