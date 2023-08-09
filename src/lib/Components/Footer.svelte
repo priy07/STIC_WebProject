73f160938841a7eb6b4e81ca3375409428658710
Om Chhayala <114079215+Om-Chhayala@users.noreply.github.com>
2023-08-09 18:58:27 +0530

    Update Footer.svelte
    - check
    - linkedin links added ( pre & vice pre )

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -35,10 +35,10 @@
             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase text-white">Contact Us</h2>
             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                 <li class="mb-4">
-                    <a href="#" class="hover:text-white">President</a>
+                    <a href="https://www.linkedin.com/in/ishitaporwal/" class="hover:text-white">President</a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:text-white">Vide-President</a>
+                    <a href="https://www.linkedin.com/in/jainaviral/" class="hover:text-white">Vide-President</a>
                 </li>
                 <li class="mb-4">
                     <a href="#" class="hover:text-white">Stic Email</a>
